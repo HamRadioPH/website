c1e1d68daed874fe5ffbf0e1f2a31f5b3927e46e
Juan Caser <jan.caser@korlanda.com>
2019-03-20 17:20:04 +0800

    Updated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,6 +13,22 @@
                 $(this).css('width','95%');
             }
         });
+
+        /**
+         * Read time
+         */
+        var content_wrapper = $('article').find('.post-content');
+        if(content_wrapper.length > 0){
+            var word_count = 0;
+            content_wrapper.find('p').each(function(){
+                word_count+=$(this).text().split(' ').length;
+            });
+            if(word_count > 0){
+                var time_in_mins = Math.floor(word_count / 228) + 1;
+                $('article').find('.reading-time').text(' - ' + time_in_mins + ' mins read');
+            }
+
+        }
     });
 
     $(window).scroll(function(){
