f8ac3eb7644696c232a69af435189c34aa150a7b
Juan Caser <caserjan@gmail.com>
2019-08-10 13:29:49 +0800

    FIx carousel width on production server

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,16 @@ $(function () {
     });
 
     $('.lightbox').fancybox();
+    
+    resize_carousel();
+
+    $('window').resize(function(){
+        resize_carousel();
+    });
+
+    function resize_carousel(){
+        if($('#carousel-wrapper').length > 0 && $('.content-box').length > 0){
+            $('#carousel-wrapper').find('.carousel-inner').width($('.content-box').innerWidth() - 280);
+        }
+    }
 })
