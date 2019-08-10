8216beca194191bec87cf2b0e49e262e9cd851a2
Juan Caser <caserjan@gmail.com>
2019-08-10 09:39:47 +0800

    Added fancybox JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,7 @@ $(function () {
     'use strict'
     $('[data-toggle="offcanvas"]').on('click', function () {
         $('.offcanvas-collapse').toggleClass('open')
-    })
+    });
+
+    $('.lightbox').fancybox();
 })
